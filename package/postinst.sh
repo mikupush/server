95ffe765098f848059c5065b70700f16c1802ba0
MagonxESP <janma.360@gmail.com>
2025-09-28 15:25:43 +0200

    chore: bump version to 0.0.5 and update permissions in postinst script

--- package/postinst.sh
+++ package/postinst.sh
@@ -14,9 +14,11 @@ chmod 775 /etc/io.mikupush.server
 chmod -R 0664 /etc/io.mikupush.server/*
 chmod 775 /var/lib/io.mikupush.server
 chmod 775 /var/log/io.mikupush.server
-chmod -R 0664 /var/log/io.mikupush.server/*
 chmod 775 /usr/share/io.mikupush.server
-chmod -R 0664 /usr/share/io.mikupush.server/*
+chmod 775 /usr/share/io.mikupush.server/static
+chmod 775 /usr/share/io.mikupush.server/templates
+chmod -R 0664 /usr/share/io.mikupush.server/static/*
+chmod -R 0664 /usr/share/io.mikupush.server/templates/*
 chown -R "$USER:$USER" /var/lib/io.mikupush.server
 chown -R "$USER:$USER" /var/log/io.mikupush.server
 chown -R "$USER:$USER" /usr/share/io.mikupush.server
