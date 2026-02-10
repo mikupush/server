d8f27a247e21f14ce7e8f24b794c7413c252589a
MagonxESP <janma.360@gmail.com>
2026-02-10 21:54:48 +0100

    chore: update file_info and openapi

---
+++ src/model/file_info.rs
@@ -0,0 +1,39 @@
+use std::path::PathBuf;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use chrono::NaiveDateTime;
+use crate::model::FileUpload as DomainFileUpload;
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum FileStatus {
+    WaitingForUpload,
+    Uploaded
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FileInfo {
+    pub id: Uuid,
+    pub name: String,
+    pub mime_type: String,
+    pub size: i64,
+    pub uploaded_at: NaiveDateTime,
+    pub status: FileStatus,
+    pub expires_at: Option<NaiveDateTime>,
+}
+
+impl FileInfo {
+    pub fn from_file_upload(file_upload: &DomainFileUpload, path: PathBuf) -> Self {
+        Self {
+            id: file_upload.id,
+            name: file_upload.name.clone(),
+            mime_type: file_upload.mime_type.clone(),
+            size: file_upload.size,
+            uploaded_at: file_upload.uploaded_at,
+            status: match path.exists() {
+                true => FileStatus::Uploaded,
+                false => FileStatus::WaitingForUpload,
+            },
+            expires_at: file_upload.expires_at,
+        }
+    }
+}
