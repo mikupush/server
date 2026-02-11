8f882b5d901dcf1dd14e7acd45ab0eaa0cf6ccf0
MagonxESP <janma.360@gmail.com>
2026-02-11 22:58:30 +0100

    refactor: prioritize environment variables over YAML in config loading

--- src/config/settings.rs
+++ src/config/settings.rs
@@ -37,24 +37,24 @@ impl DataBase {
         let default = Self::default();
 
         Self {
-            host: log_yaml_config("database.host", yaml.database.host)
-                .or_else(|| env.database.host)
+            host: env.database.host
+                .or_else(|| log_yaml_config("database.host", yaml.database.host))
                 .or_else(|| log_default_config("database.host", Some(default.host)))
                 .unwrap(),
-            port: log_yaml_config("database.port", yaml.database.port)
-                .or_else(|| env.database.port)
+            port: env.database.port
+                .or_else(|| log_yaml_config("database.port", yaml.database.port))
                 .or_else(|| log_default_config("database.port", Some(default.port)))
                 .unwrap(),
-            database: log_yaml_config("database.database", yaml.database.database)
-                .or_else(|| env.database.database)
+            database: env.database.database
+                .or_else(|| log_yaml_config("database.database", yaml.database.database))
                 .or_else(|| log_default_config("database.database", Some(default.database)))
                 .unwrap(),
-            user: log_yaml_config("database.user", yaml.database.user)
-                .or_else(|| env.database.user)
+            user: env.database.user
+                .or_else(|| log_yaml_config("database.user", yaml.database.user))
                 .or_else(|| log_default_config("database.user", Some(default.user)))
                 .unwrap(),
-            password: log_yaml_config("database.password", yaml.database.password)
-                .or_else(|| env.database.password)
+            password: env.database.password
+                .or_else(|| log_yaml_config("database.password", yaml.database.password))
                 .or_else(|| log_default_config("database.password", Some(default.password)))
                 .unwrap(),
         }
@@ -89,20 +89,20 @@ impl LoggingConfig {
     pub fn from(yaml: YamlSettings, env: EnvSettings) -> Self {
         let default = Self::default();
         Self {
-            level: log_yaml_config("log.level", yaml.log.level)
-                .or_else(|| env.log.level)
+            level: env.log.level
+                .or_else(|| log_yaml_config("log.level", yaml.log.level))
                 .or_else(|| log_default_config("log.level", Some(default.level)))
                 .unwrap(),
-            file_prefix: log_yaml_config("log.file_prefix", yaml.log.file_prefix)
-                .or_else(|| env.log.file_prefix)
+            file_prefix: env.log.file_prefix
+                .or_else(|| log_yaml_config("log.file_prefix", yaml.log.file_prefix))
                 .or_else(|| log_default_config("log.file_prefix", Some(default.file_prefix)))
                 .unwrap(),
-            directory: log_yaml_config("log.directory", yaml.log.directory)
-                .or_else(|| env.log.directory)
+            directory: env.log.directory
+                .or_else(|| log_yaml_config("log.directory", yaml.log.directory))
                 .or_else(|| log_default_config("log.directory", Some(default.directory)))
                 .unwrap(),
-            json: log_yaml_config("log.json", yaml.log.json)
-                .or_else(|| env.log.json)
+            json: env.log.json
+                .or_else(|| log_yaml_config("log.json", yaml.log.json))
                 .or_else(|| log_default_config("log.json", Some(default.json)))
                 .unwrap(),
         }
@@ -132,20 +132,20 @@ impl Server {
     pub fn from(yaml: YamlSettings, env: EnvSettings) -> Self {
         let default = Self::default();
         Self {
-            host: log_yaml_config("server.host", yaml.server.host)
-                .or_else(|| env.server.host)
+            host: env.server.host
+                .or_else(|| log_yaml_config("server.host", yaml.server.host))
                 .or_else(|| log_default_config("server.host", Some(default.host)))
                 .unwrap(),
-            port: log_yaml_config("server.port", yaml.server.port)
-                .or_else(|| env.server.port)
+            port: env.server.port
+                .or_else(|| log_yaml_config("server.port", yaml.server.port))
                 .or_else(|| log_default_config("server.port", Some(default.port)))
                 .unwrap(),
-            static_directory: log_yaml_config("server.static_directory", yaml.server.static_directory)
-                .or_else(|| env.server.static_directory)
+            static_directory: env.server.static_directory
+                .or_else(|| log_yaml_config("server.static_directory", yaml.server.static_directory))
                 .or_else(|| log_default_config("server.static_directory", Some(default.static_directory)))
                 .unwrap(),
-            templates_directory: log_yaml_config("server.templates_directory", yaml.server.templates_directory)
-                .or_else(|| env.server.templates_directory)
+            templates_directory: env.server.templates_directory
+                .or_else(|| log_yaml_config("server.templates_directory", yaml.server.templates_directory))
                 .or_else(|| log_default_config("server.templates_directory", Some(default.templates_directory)))
                 .unwrap(),
         }
@@ -176,18 +176,18 @@ impl Upload {
     pub fn from(yaml: YamlSettings, env: EnvSettings) -> Self {
         let default = Self::default();
         Self {
-            max_size: log_yaml_config("upload.max_size", yaml.upload.max_size)
-                .or_else(|| env.upload.max_size)
+            max_size: env.upload.max_size
+                .or_else(|| log_yaml_config("upload.max_size", yaml.upload.max_size))
                 .or_else(|| log_default_config("upload.max_size", default.max_size)),
-            directory: log_yaml_config("upload.directory", yaml.upload.directory)
-                .or_else(|| env.upload.directory)
+            directory: env.upload.directory
+                .or_else(|| log_yaml_config("upload.directory", yaml.upload.directory))
                 .or_else(|| log_default_config("upload.directory", Some(default.directory)))
                 .unwrap(),
-            expires_in_seconds: log_yaml_config("upload.expires_in_seconds", yaml.upload.expires_in_seconds)
-                .or_else(|| env.upload.expires_in_seconds)
+            expires_in_seconds: env.upload.expires_in_seconds
+                .or_else(|| log_yaml_config("upload.expires_in_seconds", yaml.upload.expires_in_seconds))
                 .or_else(|| log_default_config("upload.expires_in_seconds", default.expires_in_seconds)),
-            expiration_cleanup_interval_seconds: log_yaml_config("upload.expiration_cleanup_interval_seconds", yaml.upload.expiration_cleanup_interval_seconds)
-                .or_else(|| env.upload.expiration_cleanup_interval_seconds)
+            expiration_cleanup_interval_seconds: env.upload.expiration_cleanup_interval_seconds
+                .or_else(|| log_yaml_config("upload.expiration_cleanup_interval_seconds", yaml.upload.expiration_cleanup_interval_seconds))
                 .or_else(|| log_default_config("upload.expiration_cleanup_interval_seconds", Some(default.expiration_cleanup_interval_seconds)))
                 .unwrap(),
         }
@@ -312,5 +312,160 @@ impl Default for Settings {
             database: DataBase::default(),
             upload: Upload::default(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::config::env::{EnvDataBase, EnvLoggingConfig, EnvServer, EnvUpload};
+    use crate::config::yaml::{YamlDataBase, YamlLoggingConfig, YamlServer, YamlUpload};
+
+    #[test]
+    fn test_server_config_precedence() {
+        // 1. Env > Yaml
+        let mut env = EnvSettings {
+            server: EnvServer::default(),
+            log: EnvLoggingConfig::default(),
+            database: EnvDataBase::default(),
+            upload: EnvUpload::default(),
+        };
+        env.server.host = Some("env-host".to_string());
+        env.server.port = Some(9090);
+
+        let mut yaml = YamlSettings::default();
+        yaml.server.host = Some("yaml-host".to_string());
+        yaml.server.port = Some(8080);
+
+        let server = Server::from(yaml.clone(), env.clone());
+        assert_eq!(server.host, "env-host");
+        assert_eq!(server.port, 9090);
+
+        // 2. Yaml > Default (Env is None)
+        let env_empty = EnvSettings {
+            server: EnvServer::default(),
+            log: EnvLoggingConfig::default(),
+            database: EnvDataBase::default(),
+            upload: EnvUpload::default(),
+        };
+
+        let server = Server::from(yaml.clone(), env_empty.clone());
+        assert_eq!(server.host, "yaml-host");
+        assert_eq!(server.port, 8080);
+
+        // 3. Default (Env and Yaml are None)
+        let yaml_empty = YamlSettings::default();
+        let server = Server::from(yaml_empty, env_empty);
+        assert_eq!(server.host, "0.0.0.0");
+        assert_eq!(server.port, 8080);
+    }
+
+    #[test]
+    fn test_database_config_precedence() {
+        let mut env = EnvSettings {
+            server: EnvServer::default(),
+            log: EnvLoggingConfig::default(),
+            database: EnvDataBase::default(),
+            upload: EnvUpload::default(),
+        };
+        env.database.host = Some("env-db-host".to_string());
+        
+        let mut yaml = YamlSettings::default();
+        yaml.database.host = Some("yaml-db-host".to_string());
+
+        // Env > Yaml
+        let db = DataBase::from(yaml.clone(), env.clone());
+        assert_eq!(db.host, "env-db-host");
+
+        // Yaml > Default
+        let env_empty = EnvSettings {
+            server: EnvServer::default(),
+            log: EnvLoggingConfig::default(),
+            database: EnvDataBase::default(),
+            upload: EnvUpload::default(),
+        };
+        let db = DataBase::from(yaml.clone(), env_empty.clone());
+        assert_eq!(db.host, "yaml-db-host");
+
+        // Default
+        let yaml_empty = YamlSettings::default();
+        let db = DataBase::from(yaml_empty, env_empty);
+        assert_eq!(db.host, "localhost");
+    }
+
+    #[test]
+    fn test_logging_config_precedence() {
+        let mut env = EnvSettings {
+            server: EnvServer::default(),
+            log: EnvLoggingConfig::default(),
+            database: EnvDataBase::default(),
+            upload: EnvUpload::default(),
+        };
+        env.log.file_prefix = Some("env-prefix".to_string());
+        env.log.json = Some(true);
+
+        let mut yaml = YamlSettings::default();
+        yaml.log.file_prefix = Some("yaml-prefix".to_string());
+        yaml.log.json = Some(false);
+
+        // Env > Yaml
+        let log = LoggingConfig::from(yaml.clone(), env.clone());
+        assert_eq!(log.file_prefix, "env-prefix");
+        assert_eq!(log.json, true);
+
+        // Yaml > Default
+        let env_empty = EnvSettings {
+            server: EnvServer::default(),
+            log: EnvLoggingConfig::default(),
+            database: EnvDataBase::default(),
+            upload: EnvUpload::default(),
+        };
+        let log = LoggingConfig::from(yaml.clone(), env_empty.clone());
+        assert_eq!(log.file_prefix, "yaml-prefix");
+        assert_eq!(log.json, false);
+
+        // Default
+        let yaml_empty = YamlSettings::default();
+        let log = LoggingConfig::from(yaml_empty, env_empty);
+        assert_eq!(log.file_prefix, "server");
+        assert_eq!(log.json, false);
+    }
+
+    #[test]
+    fn test_upload_config_precedence() {
+        let mut env = EnvSettings {
+            server: EnvServer::default(),
+            log: EnvLoggingConfig::default(),
+            database: EnvDataBase::default(),
+            upload: EnvUpload::default(),
+        };
+        env.upload.directory = Some("env-data".to_string());
+        env.upload.max_size = Some(100);
+
+        let mut yaml = YamlSettings::default();
+        yaml.upload.directory = Some("yaml-data".to_string());
+        yaml.upload.max_size = Some(200);
+
+        // Env > Yaml
+        let upload = Upload::from(yaml.clone(), env.clone());
+        assert_eq!(upload.directory, "env-data");
+        assert_eq!(upload.max_size, Some(100));
+
+        // Yaml > Default
+        let env_empty = EnvSettings {
+            server: EnvServer::default(),
+            log: EnvLoggingConfig::default(),
+            database: EnvDataBase::default(),
+            upload: EnvUpload::default(),
+        };
+        let upload = Upload::from(yaml.clone(), env_empty.clone());
+        assert_eq!(upload.directory, "yaml-data");
+        assert_eq!(upload.max_size, Some(200));
+
+        // Default
+        let yaml_empty = YamlSettings::default();
+        let upload = Upload::from(yaml_empty, env_empty);
+        assert_eq!(upload.directory, "data");
+        assert_eq!(upload.max_size, None);
     }
 }
